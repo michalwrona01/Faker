2724fe5e99896baa17c0bfc372631613edfc0e80
Michał Wrona <wronamichal01@gmail.com>
2023-11-05 21:42:48 +0100

    feat(urls): HTTPS generator class.

--- faker_app/urls.py
+++ faker_app/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.urls import include, path, re_path
 from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -29,6 +36,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
+    generator_class=BothHttpAndHttpsSchemaGenerator,
     permission_classes=[permissions.AllowAny],
 )
 
